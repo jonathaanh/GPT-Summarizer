2b2072997926a8a644fe8e0ec1e4c5d0f9682a5f
Jonathan Hsu <johnnhsu1031@gmail.com>
2022-09-26 15:09:59 -0700

    update api key and app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import os
 
 import openai
+import textwrap
 from flask import Flask, redirect, render_template, request, url_for
 
 app = Flask(__name__)
@@ -10,10 +11,10 @@ openai.api_key = os.getenv("OPENAI_API_KEY")
 @app.route("/", methods=("GET", "POST"))
 def index():
     if request.method == "POST":
-        animal = request.form["animal"]
+        text = request.form["text"]
         response = openai.Completion.create(
             model="text-davinci-002",
-            prompt=generate_prompt(animal),
+            prompt=generate_prompt(text),
             temperature=0.6,
         )
         return redirect(url_for("index", result=response.choices[0].text))
@@ -22,14 +23,15 @@ def index():
     return render_template("index.html", result=result)
 
 
-def generate_prompt(animal):
-    return """Suggest three names for an animal that is a superhero.
+def generate_prompt(text):
+    return """Write a concise summary of the following:
+    {}
+    CONCISE SUMMARY:"""
 
-Animal: Cat
-Names: Captain Sharpclaw, Agent Fluffball, The Incredible Feline
-Animal: Dog
-Names: Ruff the Protector, Wonder Canine, Sir Barks-a-Lot
-Animal: {}
-Names:""".format(
-        animal.capitalize()
-    )
+def open_file(filepath):
+	with open(filepath, 'r', encoding='utf-8') as infile:
+		return infile.read()
+
+def save_file(content, filepath):
+	with open(filepath, 'w', encoding='utf-8') as outfile:
+		outfile.write(content)
