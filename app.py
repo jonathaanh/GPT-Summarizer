99de5c4ee323cc8acc3c0137d52e28ee794ffee7
Jonathan Hsu <johnnhsu1031@gmail.com>
2022-09-28 14:29:50 -0700

    Add recursive summary

--- app.py
+++ app.py
@@ -12,18 +12,9 @@ openai.api_key = os.getenv("OPENAI_API_KEY")
 def index():
     if request.method == "POST":
         text = request.form["text"]
-        response = openai.Completion.create(
-            model="text-davinci-002",
-            prompt=generate_prompt(text),
-            temperature=0.6,
-            max_tokens=2000,
-            top_p=1.0,
-            frequency_penalty=0.25,
-            presence_penalty=0.0,
-            stop=['<<END>>']
-        )
+        response = recursiveSummary(text)
         print(response)
-        return redirect(url_for("index", result=response.choices[0].text))
+        return redirect(url_for("index", result=response))
 
     result = request.args.get("result")
     return render_template("index.html", result=result)
@@ -32,6 +23,24 @@ def index():
 def generate_prompt(text):
     return """Write a concise detailed summary of the following:{}
     CONCISE DETAILED SUMMARY:""".format(text)
+
+def recursiveSummary(text):
+    chunks = textwrap.wrap(text,2000)
+    output = list()
+    for chunk in chunks:
+        response = openai.Completion.create(
+            model="text-davinci-002",
+            prompt=generate_prompt(chunk),
+            temperature=0.6,
+            max_tokens=2000,
+            top_p=1.0,
+            frequency_penalty=0.25,
+            presence_penalty=0.0,
+            stop=['<<END>>']
+        )
+        output.append(response.choices[0].text)
+    return output
+    
 
 def open_file(filepath):
 	with open(filepath, 'r', encoding='utf-8') as infile:
